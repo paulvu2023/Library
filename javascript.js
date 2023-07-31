e1bb4556f0d2c9cf61ad28e03334f7a22e30c541
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-31 16:22:34 -0700

    Replace pages with read status

--- javascript.js
+++ javascript.js
@@ -3,25 +3,25 @@ let bookname = undefined;
 let author = undefined;
 let pages = undefined;
 
-function Book(name, author, pages) {
+function Book(name, author, read) {
   this.name = name;
   this.author = author;
-  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary(name, author, pages) {
-  let book = new Book(name, author, pages);
+function addBookToLibrary(name, author, read) {
+  let book = new Book(name, author, read);
   myLibrary.push(book);
 }
 
 document.querySelector('form').onsubmit = () => {
   bookname = document.querySelector('#book').value;
   author = document.querySelector('#author').value;
-  pages = document.querySelector('#pages').value;
+  read = document.querySelector('#read').value;
   addBookToLibrary(bookname, author, pages);
   bookname = document.querySelector('#book').value = '';
   author = document.querySelector('#author').value = '';
-  pages = document.querySelector('#pages').value = '';
+  pages = document.querySelector('#read').value = '';
   console.log(myLibrary);
   return false;
 }
