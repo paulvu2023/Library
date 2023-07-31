7592f5907e3f495a296c791cac5f485114c37dfa
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-31 13:50:46 -0700

    Turn off autocomplete for all form elements

--- javascript.js
+++ javascript.js
@@ -7,3 +7,11 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+document.querySelector('form').onsubmit = () => {
+  const bookname = document.querySelector('#book').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+}
+
+console.log(bookname, author, pages);
